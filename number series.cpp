e588061fd95f8b239ac4e91f9c873c3acc81ef08
suvihas <129199468+suvishas@users.noreply.github.com>
2023-08-09 09:29:34 +0530

    Add files via upload

---
+++ number series.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int i,n;
+	printf("enter the value n:");
+	scanf("%d",&n);
+	printf("nunbers series:");
+	for(i=1;i<=n;i++)
+	{
+		printf("%d",i);
+	}
+	return 0;
+}
